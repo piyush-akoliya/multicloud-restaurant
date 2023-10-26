921d98420b53fe21f5983aa936a49fd40ee67b1f
boza <bh327148@dal.ca>
2023-10-25 23:36:41 -0300

    changes in UI

--- CustomerApp/Frontend/src/list-restaurant/RestaurantList.js
+++ CustomerApp/Frontend/src/list-restaurant/RestaurantList.js
@@ -1,9 +1,28 @@
+// RestaurantList.js
 import React from 'react';
 import RestaurantCard from './RestaurantCard';
 import restaurantsData from './restaurantData';
 import './styles.css';
 
-const RestaurantList = () => {   return (     <div className="restaurant-list">      {restaurantsData.map((restaurant) => (        <RestaurantCard          key={restaurant.id}          id={restaurant.id}          name={restaurant.name}          location={restaurant.location}          operationHours={restaurant.operationHours}          numOfTables={restaurant.numOfTables}          foodMenu={restaurant.foodMenu}        /> ))} </div>  ); };
-
+const RestaurantList = () => {
+  return (
+    <div className="list-container">
+      <h1>List of Restaurants</h1>
+      <div className="restaurants">
+        {restaurantsData.map((restaurant) => (
+          <RestaurantCard
+            key={restaurant.id}
+            id={restaurant.id}
+            name={restaurant.name}
+            location={restaurant.location}
+            operationHours={restaurant.operationHours}
+            numOfTables={restaurant.numOfTables}
+            foodMenu={restaurant.foodMenu}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default RestaurantList;
