85c00ba4591346daddc65f20eb8ead914b7ce76f
boza <bh327148@dal.ca>
2023-10-27 22:22:26 -0300

    all changes of list of restaurants

--- CustomerApp/Frontend/src/App.js
+++ CustomerApp/Frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Router from "./Routes";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import ScrollToTop from "./utils/ScrollToTop";
 import Login from "./auth/login";
 import Signup from "./auth/signup";
 import First from "./auth/first";
 import DemoPage1 from "./views/DemoPage1";
 import DemoPage2 from "./views/DemoPage2";
+import NavbarLayout from './utils/NavbarLayout';
+import RestaurantDetails from './list-restaurant/RestaurantDetails';
+import RestaurantList from './list-restaurant/RestaurantList';
+import './list-restaurant/styles.css';
+import Footer from './utils/Footer';
 
 export function isLoggedIn() {
   const token = localStorage.getItem("userData");
@@ -21,6 +26,7 @@ function App() {
   return (
     <BrowserRouter>
       <ScrollToTop>
+      <NavbarLayout /> 
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
@@ -28,7 +34,10 @@ function App() {
           <Route path="/*" element={<Navigate to="/first" />} />
           <Route path = '/DemoPage1' element = <Auth><DemoPage1/></Auth> />
           <Route path = '/DemoPage2' element = <Auth><DemoPage2/></Auth> />
+          <Route path="/restaurantList" element={<RestaurantList />} />
+          <Route path="/restaurant/:id" element={<RestaurantDetails />} />
         </Routes>
+        <Footer />
       </ScrollToTop>
     </BrowserRouter>
   );
--- CustomerApp/Frontend/src/Routes.js
+++ CustomerApp/Frontend/src/Routes.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
+import Login from "./auth/login";
+import Signup from "./auth/signup";
+import First from "./auth/first";
 import DemoPage1 from './views/DemoPage1';
 import DemoPage2 from './views/DemoPage2';
 import NavbarLayout from './utils/NavbarLayout';
@@ -15,6 +18,9 @@ const Router = () => {
     <>
     <NavbarLayout /> 
     <Routes location={location} key={location.pathname}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/first" element={<First />} />
       <Route path="/DemoPage1" element={<DemoPage1 />} />
       <Route path="/DemoPage2" element={<DemoPage2 />} />
       <Route path="/restaurantList" element={<RestaurantList />} />
