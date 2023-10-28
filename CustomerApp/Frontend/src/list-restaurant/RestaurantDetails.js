27c3a14bf04d499efdeec0450977795e74988144
boza <bh327148@dal.ca>
2023-10-27 22:34:47 -0300

    Formatting the code with comments

--- CustomerApp/Frontend/src/list-restaurant/RestaurantDetails.js
+++ CustomerApp/Frontend/src/list-restaurant/RestaurantDetails.js
@@ -1,19 +1,28 @@
+// Importing necessary modules from React, axios, and react-router-dom, as well as the DetailsCard component and Material-UI components
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import DetailsCard from './DetailsCard';
+import DetailsCard from './DetailsCard'; // Importing the DetailsCard component
 import { Grid, Typography, Box, Card, CardContent, Paper } from '@mui/material';
 
+// RestaurantDetails component to display detailed information about a specific restaurant
 const RestaurantDetails = () => {
+  // Using the useParams hook to get the id from the route parameters
   const { id } = useParams();
+  // Initializing the restaurant state
   const [restaurant, setRestaurant] = useState(null);
 
+  // Using the useEffect hook to fetch restaurant data
   useEffect(() => {
     const fetchRestaurantData = async () => {
       try {
+        // Fetching data from the provided API endpoint
         const response = await axios.get('https://471h7c20q8.execute-api.us-east-1.amazonaws.com/Test-1/list-of-restaurants');
+        // Parsing the received data
         const data = JSON.parse(response.data.body.replace(/"(\s+)(\w+)(?=")/g, '"$2'));
+        // Finding the selected restaurant based on the id
         const selectedRestaurant = data.find((item) => item.restaurant_id === id);
+        // Setting the restaurant state
         setRestaurant(selectedRestaurant);
       } catch (error) {
         console.error('Error fetching data: ', error);
@@ -23,10 +32,12 @@ const RestaurantDetails = () => {
     fetchRestaurantData();
   }, [id]);
 
+  // Rendering a loading message if the restaurant data is not yet fetched
   if (!restaurant) {
     return <div>Loading...</div>;
   }
 
+  // Rendering the detailed information about the restaurant
   return (
     <Box sx={{ padding: 4 }}>
       <Typography variant="h4" align="center" gutterBottom>
@@ -34,6 +45,7 @@ const RestaurantDetails = () => {
       </Typography>
       <Card sx={{ marginBottom: 4 }}>
         <CardContent>
+          {/* Displaying the restaurant's location, operation hours, and number of tables */}
           <Typography variant="subtitle1" gutterBottom>
             <b>Location:</b> {restaurant.restaurant_location}
           </Typography>
@@ -51,6 +63,7 @@ const RestaurantDetails = () => {
           </Typography>
         </CardContent>
       </Card>
+      {/* Displaying the restaurant's food menu using the DetailsCard component */}
       <Grid container spacing={3}>
         {restaurant?.restaurant_food_menu?.map((data, i) => (
           <Grid item sm={4} key={i}>
@@ -64,4 +77,5 @@ const RestaurantDetails = () => {
   );
 };
 
+// Exporting the RestaurantDetails component
 export default RestaurantDetails;
