d3174f6d7d0017cf29d8780cf4c7017c808c48b1
ramandeep <rm661081@dal.ca>
2023-11-10 21:27:47 -0400

    Added login sign up for partner app

--- PartnerApp/Frontend/src/Routes.js
+++ PartnerApp/Frontend/src/Routes.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
+import Login from "./auth/login";
+import Signup from "./auth/signup";
 
 import Demo from "./views/Demo";
 import NavbarLayout from "./utils/NavbarLayout";
@@ -9,6 +11,9 @@ const Router = () => {
   return (
     <>
       <Routes location={location} key={location.pathname}>
+      <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<NavbarLayout />}>
           <Route path="/" element={<Demo />} />
         </Route>
