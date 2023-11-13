2d24ab009bf24d63d96873934ca49e5a80d2db1b
ramandeep <rm661081@dal.ca>
2023-11-13 19:22:16 -0400

    Added code for Holistic view, daily, monthly and weekly

--- PartnerApp/Frontend/src/Routes.js
+++ PartnerApp/Frontend/src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Login from "./auth/login";
 import Signup from "./auth/signup";
+import MyCalendarComponent from "./views/Demo/HolisticView/dayGrid";
 
 import Demo from "./views/Demo";
 import NavbarLayout from "./utils/NavbarLayout";
@@ -14,6 +15,7 @@ const Router = () => {
       <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/grid" element={<MyCalendarComponent />} />
         <Route path="/" element={<NavbarLayout />}>
           <Route path="/" element={<Demo />} />
         </Route>
