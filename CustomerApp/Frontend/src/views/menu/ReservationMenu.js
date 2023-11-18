d44d079b1d84e12fc8fd7eff9c0f51e4e01d3f9b
Mohammad Amin Rashid Shaikh <mh330119@dal.ca>
2023-11-18 16:11:08 -0400

    MenuReservation updated

--- CustomerApp/Frontend/src/views/menu/ReservationMenu.js
+++ CustomerApp/Frontend/src/views/menu/ReservationMenu.js
@@ -1,32 +1,59 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Grid, Card, CardContent, CardMedia, Button } from '@mui/material';
+import axios from 'axios';
 
 function MenuReservationApp() {
-  const [menuItems, setMenuItems] = useState([
-    { id: 1, category: 'Appetizers', name: 'Salad', price: 6, quantity: 0, ingredients: 'Lettuce, Tomatoes, Cucumbers, Dressing', discount: 10 },
-    { id: 2, category: 'Appetizers', name: 'Mozzarella Sticks', price: 8, quantity: 0, ingredients: 'Mozzarella Cheese, Breadcrumbs, Marinara Sauce', discount: 15 },
-    { id: 3, category: 'Appetizers', name: 'Bruschetta', price: 7, quantity: 0, ingredients: 'Chopped Tomatoes, Basil, Garlic sauce, Olive Oil', discount: 5 },
-    { id: 4, category: 'Main Course', name: 'Burger', price: 10, quantity: 0, ingredients: 'Beef Patty, Lettuce, Tomato, Cheese, Bun', discount: 20 },
-    { id: 5, category: 'Main Course', name: 'Pizza', price: 12, quantity: 0, ingredients: 'Tomato Sauce, Cheese, Pepperoni, Dough', discount: 25 },
-    { id: 6, category: 'Main Course', name: 'Pasta', price: 8, quantity: 0, ingredients: 'Pasta, Marinara Sauce, Parmesan Cheese', discount: 10 },
-    { id: 7, category: 'Desserts', name: 'Cheesecake', price: 5, quantity: 0, ingredients: 'Cream Cheese, Sugar', discount: 15 },
-    { id: 8, category: 'Desserts', name: 'Chocolate Cake', price: 6, quantity: 0, ingredients: 'Chocolate, Flour, Sugar, Eggs', discount: 30 },
-    { id: 9, category: 'Desserts', name: 'Ice Cream', price: 4, quantity: 0, ingredients: 'Milk, Sugar, Cream', discount: 5 },
-  ]);
+  const [menuItems, setMenuItems] = useState([]);
 
-  const handleQuantityChange = (itemId, quantity) => {
-    setMenuItems((prevItems) =>
-      prevItems.map((item) =>
-        item.id === itemId ? { ...item, quantity } : item
-      )
-    );
+  useEffect(() => {
+    // Axios call on page load
+    axios.post('https://3ithnk2mg5.execute-api.us-east-1.amazonaws.com/dev/get-menu', {"restaurant_id": "1"})
+      .then(response => {
+        // Assuming the response data is an array of menu items
+        setMenuItems(response.data.body.restaurant_food_menu);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching menu items:', error);
+      });
+  }, []); // The empty dependency array ensures that this effect runs only once on mount
+
+  const isEntireMenuOffer = menuItems.length > 0 && menuItems[0].menu_offer !== undefined;
+
+  const getNonSlashedPrice = (item) => {
+    if (isEntireMenuOffer) {
+      return item.menu_price; // Display regular price when there's an entire menu offer
+    } else {
+      const offer = Number(item.menu_offer);
+      const discount = Number.isNaN(offer) ? 0 : offer; // Ensure offer is a valid number
+
+      return item.menu_offer
+        ? item.menu_price - (item.menu_price * (discount / 100))
+        : item.menu_price; // Display regular price when there's no offer for the item
+    }
+  };
+
+  const handleQuantityChange = (itemId, newQuantity) => {
+    // Ensure newQuantity is a valid number and greater than or equal to 0
+    newQuantity = Number.isNaN(newQuantity) || newQuantity < 0 ? 0 : newQuantity;
+
+    setMenuItems((prevItems) => {
+      const updatedItems = prevItems.map((item) =>
+        item.id === itemId ? { ...item, quantity: newQuantity } : item
+      );
+      return updatedItems;
+    });
   };
 
   const handleAddToCart = () => {
     const selectedItems = menuItems.filter((item) => item.quantity > 0);
     console.log('Selected items:', selectedItems);
     // Implement your logic to submit the reservation here
+
+    // Reset quantities after submitting
+    setMenuItems((prevItems) =>
+      prevItems.map((item) => ({ ...item, quantity: 0 }))
+    );
   };
 
   // Group menu items by category
@@ -35,46 +62,68 @@ function MenuReservationApp() {
   return (
     <Container className="mt-5">
       <h1 className="text-center">Restaurant Menu</h1>
+      {isEntireMenuOffer && (
+        <div className="text-center">
+          <h3>Special Offer: 20% off on the entire menu!</h3>
+        </div>
+      )}
       {categories.map((category) => (
         <div key={category}>
           <h2 className="text-center">{category}</h2>
-          <Row>
+          <Grid container spacing={3}>
             {menuItems
               .filter((item) => item.category === category)
               .map((item) => (
-                <Col key={item.id} md={4} className="mb-4">
+                <Grid key={item.id} item xs={12} md={4}>
                   <Card>
-                    <Card.Body>
-                      <h5 className="card-title">{item.name}</h5>
-                      <p className="card-text">${item.price}</p>
-                      <p className="card-text">Ingredients: {item.ingredients}</p>
-                      <p className="card-text">Discount: {item.discount}% off</p>
+                    <CardMedia
+                      component="img"
+                      alt={item.menu_category}
+                      height="140"
+                      image={item.menu_image} 
+                    />
+                    <CardContent>
+                      <h5>{item.menu_category}</h5>
+                      {isEntireMenuOffer && (
+                        <>
+                          <p style={{ textDecoration: 'line-through', color: 'gray' }}>
+                            ${item.menu_price}
+                          </p>
+                          <p style={{ fontWeight: 'bold', color: 'red' }}>
+                            ${getNonSlashedPrice(item)}
+                          </p>
+                        </>
+                      )}
+                      {!isEntireMenuOffer && <p>${item.menu_price}</p>}
+                      <p>Ingredients: {item.menu_ingrediants}</p>
+                      <p>Discount: {item.menu_offer}</p>
+                      <p>Availability: {item.menu_item_availability}</p>
                       <div className="text-center">
                         <Button
-                          variant="danger"
-                          className="mr-2"
+                          variant="contained"
+                          color="secondary"
                           onClick={() => handleQuantityChange(item.id, item.quantity - 1)}
                         >
                           -
                         </Button>
-                        <span>{item.quantity}</span>
+                        <span style={{ margin: '0 10px' }}>{item.quantity}</span>
                         <Button
-                          variant="success"
-                          className="ml-2"
+                          variant="contained"
+                          color="primary"
                           onClick={() => handleQuantityChange(item.id, item.quantity + 1)}
                         >
                           +
                         </Button>
                       </div>
-                    </Card.Body>
+                    </CardContent>
                   </Card>
-                </Col>
+                </Grid>
               ))}
-          </Row>
+          </Grid>
         </div>
       ))}
       <div className="text-center mt-4">
-        <Button variant="primary" size="lg" onClick={handleAddToCart}>
+        <Button variant="contained" color="primary" size="large" onClick={handleAddToCart}>
           Submit Reservation
         </Button>
       </div>
@@ -83,3 +132,4 @@ function MenuReservationApp() {
 }
 
 export default MenuReservationApp;
+
