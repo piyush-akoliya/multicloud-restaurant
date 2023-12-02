0ec1551ce4d1760dd724238f9a3206f0758e0ab0
Bhavisha Oza <bh327148@dal.ca>
2023-12-02 02:10:35 -0400

    Code formation

--- AdminApp/Frontend/src/views/Restaurant/TopRestaurants.js
+++ AdminApp/Frontend/src/views/Restaurant/TopRestaurants.js
@@ -1,13 +1,28 @@
 import React from "react";
-import "./styles.css" 
+import "./styles.css";
+
+/**
+ * Functional component to display the top restaurants.
+ * This component renders an iframe embedding a report from Google Looker Studio.
+ * The report is expected to showcase information about top restaurants.
+ */
 function TopRestaurants() {
   return (
     <div className="App">
       <header className="App-header">
-      <iframe width="600" height="450" src="https://lookerstudio.google.com/embed/reporting/5fa8f379-3b69-45e8-8802-38212db48f98/page/aV2jD" frameborder="0" style={{ border: 0 }} allowfullscreen></iframe>
+        {/* iframe to embed the Google Looker Studio report */}
+        <iframe 
+          width="600" 
+          height="450" 
+          src="https://lookerstudio.google.com/embed/reporting/5fa8f379-3b69-45e8-8802-38212db48f98/page/aV2jD" 
+          frameBorder="0" 
+          style={{ border: 0 }} 
+          allowFullScreen 
+        >
+        </iframe>
       </header>
     </div>
   );
 }
- 
+
 export default TopRestaurants;
