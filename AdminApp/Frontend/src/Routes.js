6d865a992eaeb49c523d1691106b93a09ab0a280
ramandeep <rm661081@dal.ca>
2023-12-02 19:27:00 -0400

    Embedded looker studio for top 10 time period food was ordered

--- AdminApp/Frontend/src/Routes.js
+++ AdminApp/Frontend/src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import NavbarLayout from "./utils/NavbarLayout";
 import Home from "./views/Home";
+import TimePeriod from "./views/TimePeriod"
 const Router = () => {
   const location = useLocation();
 
@@ -10,6 +11,7 @@ const Router = () => {
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<NavbarLayout />}>
           <Route path="/home" element={<Home />} />
+          <Route path="/timeperiod" element={<TimePeriod />} />
         </Route>
       </Routes>
     </>
