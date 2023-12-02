38ab82fb11f896ad5ef324634ae6620c36766714
Bhavisha Oza <bh327148@dal.ca>
2023-12-02 19:38:37 -0400

    Report changes for top 10 restros

--- AdminApp/Frontend/src/views/Restaurant/TopRestaurants.js
+++ AdminApp/Frontend/src/views/Restaurant/TopRestaurants.js
@@ -9,17 +9,20 @@ import "./styles.css";
 function TopRestaurants() {
   return (
     <div className="App">
-      <header className="App-header">
+        <h2>
+            <br></br>
+        </h2>
+      <header className="TopRestaurants-header">
         {/* iframe to embed the Google Looker Studio report */}
-        <iframe 
-          width="600" 
-          height="450" 
-          src="https://lookerstudio.google.com/embed/reporting/5fa8f379-3b69-45e8-8802-38212db48f98/page/aV2jD" 
-          frameBorder="0" 
-          style={{ border: 0 }} 
-          allowFullScreen 
-        >
-        </iframe>
+         <iframe 
+         width="900" 
+         height="750" 
+         src="https://lookerstudio.google.com/embed/reporting/43d91b72-7bb2-4cae-9bbe-5cf635d1eaa8/page/nK5jD" 
+         frameborder="0" 
+         style={{ border: 0 }} 
+         allowfullscreen>
+
+         </iframe>
       </header>
     </div>
   );
