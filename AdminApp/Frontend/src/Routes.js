51cbed77f068a0206114e2ad85c80250a6455094
Riya <ry470536@dal.ca>
2023-12-02 01:18:53 -0400

    login page admin

--- AdminApp/Frontend/src/Routes.js
+++ AdminApp/Frontend/src/Routes.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import NavbarLayout from "./utils/NavbarLayout";
 import Home from "./views/Home";
+import Login from "./views/Login/Login";
 const Router = () => {
   const location = useLocation();
 
   return (
     <>
       <Routes location={location} key={location.pathname}>
+      
+      <Route path = '/login' element = {<Login/>}/>
         <Route path="/" element={<NavbarLayout />}>
           <Route path="/home" element={<Home />} />
         </Route>
