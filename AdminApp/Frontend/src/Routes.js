f46b638579946a3251349435b7accdd5f3eae58d
Bhavisha Oza <bh327148@dal.ca>
2023-12-02 01:54:50 -0400

    Changes according to looker studio code for top 10 restaurants

--- AdminApp/Frontend/src/Routes.js
+++ AdminApp/Frontend/src/Routes.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import NavbarLayout from "./utils/NavbarLayout";
 import Home from "./views/Home";
+import TopRestaurants from "./views/Restaurant/TopRestaurants";
+
 const Router = () => {
   const location = useLocation();
 
@@ -10,6 +12,7 @@ const Router = () => {
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<NavbarLayout />}>
           <Route path="/home" element={<Home />} />
+          <Route path="/topRestaurant" element={<TopRestaurants />} />
         </Route>
       </Routes>
     </>
---
+++ AdminApp/Frontend/src/views/Restaurant/TopRestaurants.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css" 
+function TopRestaurants() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <iframe width="600" height="450" src="https://lookerstudio.google.com/embed/reporting/5fa8f379-3b69-45e8-8802-38212db48f98/page/aV2jD" frameborder="0" style={{ border: 0 }} allowfullscreen></iframe>
+      </header>
+    </div>
+  );
+}
+ 
+export default TopRestaurants;
