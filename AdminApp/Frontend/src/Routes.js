3762d349079e196e8fd61559bad106f75373e5d0
Riya <ry470536@dal.ca>
2023-12-02 19:33:00 -0400

    food items view added

--- AdminApp/Frontend/src/Routes.js
+++ AdminApp/Frontend/src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import NavbarLayout from "./utils/NavbarLayout";
 import Home from "./views/Home";
+import FoodItems from "./views/Food_Items/FoodItems";
 const Router = () => {
   const location = useLocation();
 
@@ -10,6 +11,7 @@ const Router = () => {
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<NavbarLayout />}>
           <Route path="/home" element={<Home />} />
+          <Route path = "/fooditems" element = {<FoodItems/>}/>
         </Route>
       </Routes>
     </>
