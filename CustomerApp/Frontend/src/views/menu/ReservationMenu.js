2da019d2f5c06fecc7d4bdd9bc9a14a82ddd43cf
Mohammad Amin Rashid Shaikh <mh330119@dal.ca>
2023-12-03 20:07:23 -0400

    Edit added in MenuReservation

--- CustomerApp/Frontend/src/views/menu/ReservationMenu.js
+++ CustomerApp/Frontend/src/views/menu/ReservationMenu.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grid, Card, CardContent, CardMedia, Button } from '@mui/material';
+import {
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  Button,
+  ThemeProvider,
+  createTheme,
+} from '@mui/material';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 
 function MenuReservationApp() {
   const userId = localStorage.getItem('user_id');
   const [menuItems, setMenuItems] = useState([]);
-
   const location = useLocation();
+  const [selectedItemId, setSelectedItemId] = useState(null);
   const reservationData = location.state?.reservationData || {};
 
   useEffect(() => {
@@ -26,18 +35,24 @@ function MenuReservationApp() {
       });
   }, []);
 
+  // Basic theme setup using MUI v5
+  const theme = createTheme({
+    palette: {
+      mode: 'light', // Set 'dark' if you want a dark theme
+    },
+  });
+
   const isEntireMenuOffer =
     menuItems.length > 0 && menuItems[0].menu_offer !== undefined;
 
   const getNonSlashedPrice = (item) => {
     return {
       regularPrice: item.menu_price,
-      discountedPrice: Math.round((item.menu_price / 1.1) * 100) / 100, // Display regular price divided by 10%
+      discountedPrice: Math.round((item.menu_price / 1.1) * 100) / 100,
     };
   };
 
   const handleQuantityChange = (itemId, newQuantity) => {
-    // Ensure newQuantity is a valid number and greater than or equal to 0
     newQuantity = Number.isNaN(newQuantity) || newQuantity < 0 ? 0 : newQuantity;
 
     setMenuItems((prevItems) => {
@@ -47,6 +62,11 @@ function MenuReservationApp() {
 
       return updatedItems;
     });
+  };
+
+  const handleEditItem = (itemId) => {
+    //Storing the selected item ID
+    setSelectedItemId(itemId);
   };
 
   const handleDeleteItem = (itemId) => {
@@ -61,19 +81,15 @@ function MenuReservationApp() {
   const handleAddToCart = async () => {
     const selectedItems = menuItems.filter((item) => item.quantity > 0);
     console.log('Selected items:', selectedItems);
-    // Implement your logic to submit the reservation here
 
     console.log(reservationData['reservation_id']);
-    // Step 2: Prepare the updated data
     const updatedData = {
-      id: reservationData['reservation_id'], // The reservation ID to update
+      id: reservationData['reservation_id'],
       food_reservation: selectedItems,
-      // This is hardcoded for now, adjust as needed
       updated_by: userId,
       updated_date: new Date().toISOString(),
     };
 
-    // Step 3: Send the updated data to the cloud function
     try {
       console.log(JSON.stringify(updatedData));
       const response = await fetch(
@@ -96,108 +112,120 @@ function MenuReservationApp() {
       alert('Error updating reservation: ' + error.message);
     }
 
-    // Reset quantities after submitting
     setMenuItems((prevItems) =>
       prevItems.map((item) => ({ ...item, quantity: 0 }))
     );
   };
 
-  // Group menu items by category
   const categories = [...new Set(menuItems.map((item) => item.category))];
 
   return (
-    <Container className="mt-5">
-      <h1 className="text-center">Restaurant Menu</h1>
-      {isEntireMenuOffer && (
-        <div className="text-center">
-          <h3>Special Offer: 20% off on the entire menu!</h3>
+    <ThemeProvider theme={theme}>
+      <Container className="mt-5">
+        <h1 className="text-center">Restaurant Menu</h1>
+        {isEntireMenuOffer && (
+          <div className="text-center">
+            <h3>Special Offer: 20% off on the entire menu!</h3>
+          </div>
+        )}
+        {categories.map((category) => (
+          <div key={category}>
+            <h2 className="text-center">{category}</h2>
+            <Grid container spacing={3}>
+              {menuItems
+                .filter((item) => item.category === category)
+                .map((item) => (
+                  <Grid key={item.id} item xs={12} md={4}>
+                    <Card>
+                      <CardMedia
+                        component="img"
+                        alt={item.menu_category}
+                        height="140"
+                        image={item.menu_image}
+                      />
+                      <CardContent>
+                        <h5>{item.menu_category}</h5>
+                        {isEntireMenuOffer && (
+                          <>
+                            <p style={{ textDecoration: 'line-through', color: 'gray' }}>
+                              ${getNonSlashedPrice(item).regularPrice}
+                            </p>
+                            <p style={{ fontWeight: 'bold', color: 'red' }}>
+                              ${getNonSlashedPrice(item).discountedPrice}
+                            </p>
+                          </>
+                        )}
+                        {!isEntireMenuOffer && <p>${item.menu_price}</p>}
+                        <p>Ingredients: {item.menu_ingrediants}</p>
+                        <p>Discount: {item.menu_offer}</p>
+                        <p>
+                          Availability:{' '}
+                          {item.menu_item_availability === 'available'
+                            ? 'Available'
+                            : 'Unavailable'}
+                        </p>
+                        {item.menu_item_availability === 'available' && (
+                          <div className="text-center">
+                            <span style={{ margin: '0 50px' }}></span>
+                            <Button
+                              variant="contained"
+                              color="secondary"
+                              onClick={() => handleQuantityChange(item.id, item.quantity - 1)}
+                            >
+                              -
+                            </Button>
+                            <span style={{ margin: '0 10px' }}>{item.quantity}</span>
+                            <Button
+                              variant="contained"
+                              color="primary"
+                              onClick={() => handleQuantityChange(item.id, item.quantity + 1)}
+                            >
+                              +
+                            </Button>
+                          </div>
+                        )}
+                        {item.menu_item_availability === 'available' && (
+                          <div>
+                            <span style={{ margin: '0 0px' }}></span>
+                            <Button
+                              variant="contained"
+                              color="primary"
+                              onClick={() => handleDeleteItem(item.id)}
+                            >
+                              Delete
+                            </Button>
+                            <span style={{ margin: '0 90px' }}></span>
+                          
+                            <Button
+                              variant="contained"
+                              color="primary"
+                        
+                              onClick={() => handleEditItem(item.id)}
+                            >
+                              Edit
+                            </Button>
+                          </div>
+                        )}
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
+            </Grid>
+          </div>
+        ))}
+        <div className="text-center mt-4">
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={handleAddToCart}
+          >
+            Submit Reservation
+          </Button>
         </div>
-      )}
-      {categories.map((category) => (
-        <div key={category}>
-          <h2 className="text-center">{category}</h2>
-          <Grid container spacing={3}>
-            {menuItems
-              .filter((item) => item.category === category)
-              .map((item) => (
-                <Grid key={item.id} item xs={12} md={4}>
-                  <Card>
-                    <CardMedia
-                      component="img"
-                      alt={item.menu_category}
-                      height="140"
-                      image={item.menu_image}
-                    />
-                    <CardContent>
-                      <h5>{item.menu_category}</h5>
-                      {isEntireMenuOffer && (
-                        <>
-                          <p style={{ textDecoration: 'line-through', color: 'gray' }}>
-                            ${getNonSlashedPrice(item).regularPrice}
-                          </p>
-                          <p style={{ fontWeight: 'bold', color: 'red' }}>
-                            ${getNonSlashedPrice(item).discountedPrice}
-                          </p>
-                        </>
-                      )}
-                      {!isEntireMenuOffer && <p>${item.menu_price}</p>}
-                      <p>Ingredients: {item.menu_ingrediants}</p>
-                      <p>Discount: {item.menu_offer}</p>
-                      <p>
-                        Availability:{' '}
-                        {item.menu_item_availability === 'available'
-                          ? 'Available'
-                          : 'Unavailable'}
-                      </p>
-                      {item.menu_item_availability === 'available' && (
-                        <div className="text-center">
-                          <Button
-                            variant="contained"
-                            color="secondary"
-                            onClick={() => handleQuantityChange(item.id, item.quantity - 1)}
-                          >
-                            -
-                          </Button>
-                          <span style={{ margin: '0 10px' }}>{item.quantity}</span>
-                          <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={() => handleQuantityChange(item.id, item.quantity + 1)}
-                          >
-                            +
-                          </Button>
-                        </div>
-                      )}
-                      {item.menu_item_availability === 'available' && (
-                        <div className="text-left">
-                          <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={() => handleDeleteItem(item.id)}
-                          >
-                            Delete
-                          </Button>
-                        </div>
-                      )}
-                    </CardContent>
-                  </Card>
-                </Grid>
-              ))}
-          </Grid>
-        </div>
-      ))}
-      <div className="text-center mt-4">
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          onClick={handleAddToCart}
-        >
-          Submit Reservation
-        </Button>
-      </div>
-    </Container>
+      </Container>
+    </ThemeProvider>
   );
-}
+};
 
 export default MenuReservationApp;
